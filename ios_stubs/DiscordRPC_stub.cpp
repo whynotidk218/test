b36140c4cbef9bc7460984c3ad5bbe7d3120e3be
no <idkpkdev@gmail.com>
2026-06-15 06:57:00 +0700

    Create DiscordRPC_stub.cpp

---
+++ ios_stubs/DiscordRPC_stub.cpp
@@ -0,0 +1,8 @@
+// iOS stub: DiscordRPC không dùng trên iOS
+#include "DiscordRPC.h"
+namespace DiscordRPC {
+    void Init() {}
+    void UpdatePresence(const std::string&) {}
+    void Update() {}
+    void Shutdown() {}
+}
