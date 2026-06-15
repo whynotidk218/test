3b7d628b4ba6f497271d8c88f639c284568d4dee
no <idkpkdev@gmail.com>
2026-06-15 11:58:29 +0700

    Create AppDelegate.m

---
+++ ios/AppDelegate.m
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor blackColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
