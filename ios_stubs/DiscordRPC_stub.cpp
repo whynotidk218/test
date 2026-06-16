83a7697d22a63c5a1e87275ec46c4e1cc263dfbe
no <idkpkdev@gmail.com>
2026-06-16 09:39:20 +0700

    Update DiscordRPC_stub.cpp

--- ios_stubs/DiscordRPC_stub.cpp
+++ ios_stubs/DiscordRPC_stub.cpp
@@ -1,8 +1,32 @@
-// iOS stub: DiscordRPC không dùng trên iOS
-#include "DiscordRPC.h"
-namespace DiscordRPC {
-    void Init() {}
-    void UpdatePresence(const std::string&) {}
-    void Update() {}
-    void Shutdown() {}
+// Discord RPC stub for iOS
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#if TARGET_OS_IPHONE
+
+// Stub implementation
+void Discord_Initialize(const char* applicationId, void* handlers, int autoRegister, const char* optionalSteamId) {
+    // No-op on iOS
 }
+
+void Discord_Shutdown(void) {
+    // No-op on iOS
+}
+
+void Discord_RunCallbacks(void) {
+    // No-op on iOS
+}
+
+void Discord_UpdatePresence(void* presence) {
+    // No-op on iOS
+}
+
+void Discord_ClearPresence(void) {
+    // No-op on iOS
+}
+
+void Discord_Respond(const char* userId, int reply) {
+    // No-op on iOS
+}
+
+#endif
+#endif
