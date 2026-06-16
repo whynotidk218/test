4b9603325cb51ff61e99f197d351db54add5d6eb
no <idkpkdev@gmail.com>
2026-06-16 09:41:08 +0700

    Update Plugin_stub.cpp

--- ios_stubs/Plugin_stub.cpp
+++ ios_stubs/Plugin_stub.cpp
@@ -1,8 +1,47 @@
-// iOS stub: Plugin system không dùng trên iOS
-#include <string>
+// Plugin system stub for iOS
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#if TARGET_OS_IPHONE
 
-// casioemu.cpp gọi LoadPlugins() khi __ANDROID__ hoặc _WIN32
-// Trên iOS không có plugin nên stub rỗng
-namespace casioemu {
-    void LoadPlugins() {}
+#include <stdio.h>
+
+class PluginManager {
+public:
+    static PluginManager& GetInstance() {
+        static PluginManager instance;
+        return instance;
+    }
+    
+    void Initialize(void) {
+        printf("Plugin system disabled on iOS\n");
+    }
+    
+    void Shutdown(void) {
+        // No-op
+    }
+    
+    void LoadPlugins(const char* path) {
+        // No-op on iOS
+    }
+    
+private:
+    PluginManager() = default;
+};
+
+// C API stubs
+extern "C" {
+    void Plugin_Init(void) {
+        PluginManager::GetInstance().Initialize();
+    }
+    
+    void Plugin_Shutdown(void) {
+        PluginManager::GetInstance().Shutdown();
+    }
+    
+    void Plugin_LoadFromDir(const char* dir) {
+        PluginManager::GetInstance().LoadPlugins(dir);
+    }
 }
+
+#endif
+#endif
